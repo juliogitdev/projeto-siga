85c8ccf744293c326e5fbd02159ac63689636327
juliogitdev <juliovieira035@gmail.com>
2025-06-18 10:08:55 -0300

    Alterando os nomes do projeto

--- src/main/java/com/siga/Main.java
+++ src/main/java/com/siga/Main.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.almoxarifado;
+package com.siga;
 
 /**
  *
@@ -10,6 +10,6 @@ package com.almoxarifado;
  */
 public class Main {
     public static void main (String args[]){
-        
+        System.out.println("Teste");
     }
 }
