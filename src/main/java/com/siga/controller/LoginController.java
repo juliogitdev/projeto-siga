3d34d3488b338d85f8d7b55e42b4a9fa918d64aa
juliogitdev <juliovieira035@gmail.com>
2025-06-29 09:56:00 -0300

    Adicionando listeners e verificando os campos

---
+++ src/main/java/com/siga/controller/LoginController.java
@@ -0,0 +1,47 @@
+package com.siga.controller;
+
+/**
+ *
+ * @author Julio
+ */
+
+import com.siga.view.auth.TelaLogin;
+import com.siga.dao.UsuarioDao;
+
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class LoginController{
+    private TelaLogin telaLogin;
+    private UsuarioDao usuarioDao;
+
+    public LoginController(TelaLogin tl, UsuarioDao ud) {
+        this.telaLogin = tl;
+        this.usuarioDao = ud;
+        this.telaLogin.addLoginListener(new LoginListener());
+    }
+
+    class LoginListener implements ActionListener{
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            String email = telaLogin.getLoginString();
+            String senha = telaLogin.getSenhaString();
+        
+            if(email.isEmpty() | senha.isEmpty()){
+                telaLogin.showMessage("Por favor preencha todos os campos!");
+                return;
+            }
+        
+            if(!email.contains("@") | !email.contains(".")){
+                telaLogin.showMessage("Por favor digite um e-mail válido");
+                return;
+            }
+        
+        }
+    }
+}
+    
+    
+     
+
